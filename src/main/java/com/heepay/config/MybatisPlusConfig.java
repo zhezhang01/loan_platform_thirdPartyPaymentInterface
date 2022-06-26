83993ce704ec750af2d769573d4b7c9e02ba4cbb
zhezhang01 <zzhang02@usc.edu>
2022-06-26 16:20:18 -0700

    upload all

---
+++ src/main/java/com/heepay/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.heepay.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MybatisPlus配置类
+ *
+ * @author qy
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.heepay.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+}
